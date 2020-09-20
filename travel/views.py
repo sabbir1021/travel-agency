2325b82ef3cc66dbc807ef354aa077ce1b66be0e
MahmudulHassan5809 <mahmudul.hassan240@gmail.com>
2020-09-20 23:01:02 +0600

    Convert Normal View TO Generic ListView

--- travel/views.py
+++ travel/views.py
@@ -1,18 +1,23 @@
 from django.shortcuts import render
-from .models import Package , PackageImage
+from .models import Package, PackageImage
 from django.views import View, generic
 
 
-class HomeView(View):
-    def get(self, request, *args, **kwargs):
-        packages = Package.objects.prefetch_related('images').all()
-        
-        context = {
-            'packages': packages,
-            
-        }
-        return render(request, 'index.html', context)
+class HomeView(generic.ListView):
+    model = Package
+    template_name = 'package/index.html'
+    context_object_name = 'package_list'
+    paginate_by = 10
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Home'
+        return context
+
+    def get_queryset(self):
+        qs = Package.objects.prefetch_related('images').all()
+        return qs
 
 
 def contact(request):
-    return render(request , "contact.html")
+    return render(request, "contact.html")
